42127de9dd1af38ad181635e7bf377c3c5c4ab00
igorMandala <igor@mandalaclothing.com.br>
2020-09-16 00:46:35 -0300

    adicionar tela de categorias

---
+++ src/pages/Categories.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="categories-body">
+        <span class="title">Categorias</span>
+        <div class="categories-container">
+            <CategorieCard :categorie="categorie1"></CategorieCard>
+            <CategorieCard :categorie="categorie2"></CategorieCard>
+            <CategorieCard :categorie="categorie3"></CategorieCard>
+            <CategorieCard :categorie="categorie2"></CategorieCard>
+            <CategorieCard :categorie="categorie1"></CategorieCard>
+            <CategorieCard :categorie="categorie3"></CategorieCard>
+        </div>
+    </div>
+</template>
+
+<script>
+    import CategorieCard from '../components/CategorieCard.vue'
+
+    export default {
+        name: "Categories",
+        components: {
+            CategorieCard,
+        },
+        data() {
+            return {
+                categorie1: {
+                    backgroundUrl: 'https://i.pinimg.com/564x/2a/3e/da/2a3edaa3cf5d3996a74846225e2ae5c9.jpg',
+                    title: 'Otaku'
+                },
+                categorie2: {
+                    backgroundUrl: 'https://i.pinimg.com/564x/bd/e6/62/bde662ee04fb0b517cf2610cce8fea16.jpg',
+                    title: 'Gamer'
+                },
+                categorie3: {
+                    backgroundUrl: 'https://i.pinimg.com/564x/cc/28/f9/cc28f92d03de23674705aa1b4bfb48b5.jpg',
+                    title: 'Cartoons'
+                },
+            }
+        }
+    };
+</script>
+
+<style>
+    .categories-container {
+        justify-content: space-between;
+        flex-wrap: wrap;
+        display: flex;
+        padding: 5px 25px;
+    }
+    .title{
+        font-size: 40px;
+        font-weight: bold;
+    }
+    .categories-body{
+        text-align: center;
+    }
+</style>
