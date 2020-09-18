df60449d812c4b9ca86ed6407c683ab4ce9ceb14
Lucas Ariel <ylucasx3@gmail.com>
2020-09-17 21:47:10 -0300

    gustavo gay

---
+++ src/components/product/product-slider/Item.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="content">
+
+        <img src="https://i.pinimg.com/564x/5c/45/61/5c4561d95857586852116379d86ce1fe.jpg" alt="">
+        <div class="items">
+            <span>Camisa Manga Curta Estampa Chihiro</span>
+            <span>R$ 200,00</span>
+            <span>7x de R$ 23.55 no cartão kimochism</span>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        name: 'Item'
+    }
+</script>
+
+<style scoped>
+    .content {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        padding: 0 5% 0 5%;
+        border:
+    }
+
+    img {
+        width: 70%;
+        height: 220px;
+    }
+
+    .items {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        width: 100%;
+        font-weight: normal;
+        padding: 10px;
+        color: black;
+        font-size:16px;
+    }
+
+    span:nth-child(2) {
+        font-weight: 600;
+    }
+</style>
---
+++ src/components/product/product-slider/Slider.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <carousel :navigationEnabled="true" :perPageCustom="[[300, 1],[625, 2], [800, 3],[1024, 4]]" :autoplay="true">
+      <slide><Item /></slide>
+      <slide><Item /></slide>
+      <slide><Item /></slide>
+      <slide><Item /></slide>
+      <slide><Item /></slide>
+      <slide><Item /></slide>
+      <slide><Item /></slide>
+      <slide><Item /></slide>
+      <slide><Item /></slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+
+import { Carousel, Slide } from "vue-carousel"
+import Item from './Item.vue'
+
+export default {
+  name: "Slider",
+
+  data: function () {
+    return {}
+  },
+  components: {
+    Carousel,
+    Slide,
+    Item
+  },
+};
+</script>
+
+<style>
+    #container {
+      padding: 0 60px;
+    }
+
+    .VueCarousel-slide {
+      position: relative;
+      padding-top: 50px;
+      cursor: pointer;
+      color: #fff;
+      font-size: 24px;
+      text-align: center;
+      min-height: 100px;
+    }
+
+    .label {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+</style>
