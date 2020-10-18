885e94be6cfbe077fda41c50d55e7e16da075174
yLucasx3 <ylucasx3@gmail.com>
2020-10-18 17:48:27 -0300

    feat(baseModal)

--- src/views/modals/component.vue
+++ src/views/modals/component.vue
@@ -1,32 +1,46 @@
 <template>
-  <div class="overlay" id="overlay">
-      <div class="container">
-        <div class="header">
-            <span class="title">
-                <slot name="title"></slot>
-            </span>
-            <span class="material-icons"  @click="destroy()">close</span>
-        </div>
-        <div class="content">
-            <slot name="content"></slot>
-        </div>
-        <div class="footer">
-            <slot name="footer"></slot>
-        </div>
-      </div>
-  </div>
+    <div id="modal-content">
+        <Overlay @click="destroy()" />
+        <Modal>
+            <Header v-if="$slots.title">
+                <span class="title">
+                    <slot name="title"></slot>
+                </span>
+                <span class="material-icons"  @click="destroy()">close</span>
+            </Header>
+            <Content>
+                <span 
+                    class="material-icons"  
+                    @click="destroy()"
+                    v-if="!$slots.title"
+                    >
+                        close
+                </span>
+                <slot name="content"></slot>
+            </Content>
+            <Footer v-if="$slots.footer">
+                <slot name="footer"></slot>
+            </Footer>
+        </Modal>
+    </div>
 </template>
 
 <script>
 
 import { actions } from "./store";
+import { Overlay, Modal, Header, Content, Footer } from "./styled-component";
 
 export default {
     name: "BaseModal",
+    components: {
+        Overlay,
+        Modal,
+        Header,
+        Content,
+        Footer
+    },
     methods: {
         ...actions
     }
 }
 </script>
-
-<style src="./style.css" scoped></style>
---
+++ src/views/modals/styled-component.js
@@ -0,0 +1,81 @@
+import styled from "vue-styled-components";
+
+const Overlay = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.4);
+    position: fixed;
+    justify-content: center;
+    align-items: center;
+    z-index: 9998;
+`;
+
+const Modal = styled.div`
+    min-width: 35%;
+    max-height: 75vh;
+    overflow-y: auto;
+    position: fixed;
+    margin: 0 auto;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    transition: 1s ease-in-out;
+    z-index: 9999;
+`;
+
+const Header = styled.div`
+    border-bottom: 1px solid lightgray;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    .title {
+        width: 100%;
+        text-align: center;
+        font-size: 20px;
+    }
+
+    .material-icons {
+        position: absolute;
+        margin-right: 10px;
+        cursor: pointer;
+        transition: 0.4s ease-in-out;
+    }
+
+    .material-icons:hover{
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+`;
+
+const Content = styled.div`
+    width: 100%;
+    max-height: 75%;
+
+    .material-icons {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        cursor: pointer;
+        transition: 0.4s ease-in-out;
+    }
+    
+    .material-icons:hover{
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+`;
+
+const Footer = styled.div`
+    border-top: 1px solid lightgray;
+    width: 100%;
+`;
+
+export {
+    Overlay,
+    Modal,
+    Header,
+    Content,
+    Footer
+};
